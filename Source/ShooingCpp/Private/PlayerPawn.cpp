2e43685972f0ace212da5111d1a7228e3226fe1c
Jacoby2193 <jacobyi2193@gmail.com>
2024-07-03 14:15:19 +0900

    20240703

---
+++ Source/ShooingCpp/Private/PlayerPawn.cpp
@@ -0,0 +1,66 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PlayerPawn.h"
+#include "Components/BoxComponent.h"
+
+APlayerPawn::APlayerPawn()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	// 충돌체 컴포넌트를 추가해서 Root컴포넌트로 설정
+	BoxComp = CreateDefaultSubobject<UBoxComponent>( TEXT( "BoxComp" ) );
+	SetRootComponent( BoxComp );
+	// 박스 Extent를 50으로 하고싶다.
+	BoxComp->SetBoxExtent( FVector( 50 ) );
+
+	//메시 컴포넌트를 추가해서 Root컴포넌트에 Attach
+	MeshComp = CreateDefaultSubobject<UStaticMeshComponent>( TEXT( "MeshComp" ) );
+
+	MeshComp->SetupAttachment( RootComponent );
+}
+
+// Called when the game starts or when spawned
+void APlayerPawn::BeginPlay()
+{
+	Super::BeginPlay();
+
+}
+
+// Called every frame
+void APlayerPawn::Tick( float DeltaTime )
+{
+	Super::Tick( DeltaTime );
+
+	// 좌우방향, 상하방향
+	FVector dir = FVector( 0 , h , v );
+	// dir의 길이를 1로 만들고싶다.
+	dir.Normalize();
+	// P = P0 + vt;
+	FVector p0 = GetActorLocation();
+	FVector velocity = dir * Speed;
+
+	SetActorLocation( p0 + velocity * DeltaTime );
+
+}
+
+// Called to bind functionality to input
+void APlayerPawn::SetupPlayerInputComponent( UInputComponent* PlayerInputComponent )
+{
+	Super::SetupPlayerInputComponent( PlayerInputComponent );
+
+	PlayerInputComponent->BindAxis( TEXT( "Horizontal" ) , this , &APlayerPawn::OnMyAxisHorizontal );
+	PlayerInputComponent->BindAxis( TEXT( "Vertical" ) , this , &APlayerPawn::OnMyAxisVertical );
+
+}
+
+void APlayerPawn::OnMyAxisHorizontal( float value )
+{
+	h = value;
+}
+
+void APlayerPawn::OnMyAxisVertical( float value )
+{
+	v = value;
+}
+
---
+++ Source/ShooingCpp/Public/TailActor.h
@@ -0,0 +1,38 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "TailActor.generated.h"
+
+// 살아가면서 목적지를 향해 계속 이동하고싶다.
+// - Target : 목적지
+// - Speed
+UCLASS()
+class SHOOINGCPP_API ATailActor : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ATailActor();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Cube 외형을 만들고 싶다.
+	UPROPERTY(EditAnywhere)
+	class UStaticMeshComponent* MeshComp;
+	
+	UPROPERTY(EditAnywhere)
+	class AActor* Target;
+
+	UPROPERTY(EditAnywhere)
+	float Speed = 450;
+};
